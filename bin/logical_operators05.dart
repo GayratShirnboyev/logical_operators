6ae1aa13c2a0180631c2d5a3ffba2cabf226d708
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-30 11:44:31 +0500

    main5

--- bin/logical_operators05.dart
+++ bin/logical_operators05.dart
@@ -8,5 +8,10 @@ Given two integers a, b,  check the following statement "Each of the numbers 'a'
     Returns:
         bool: answer
 */
+bool func(a, b) {
+  return 1 + a % 2 == 1 && b % 2 == 0;
+}
 
-void main() {}
+void main() {
+  print(func(7, 9));
+}
