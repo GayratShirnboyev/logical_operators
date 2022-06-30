928852cda0be3617e9e84a4bf694126d3a713f6b
G'ayrat <shirnboyevgayrat@gmail.com>
2022-06-30 22:48:23 +0500

    main14

--- bin/logical_operators14.dart
+++ bin/logical_operators14.dart
@@ -7,5 +7,11 @@ Given a two-digit integer a,  check the following statement "All digits sum is o
     Returns:
         bool: answer
 */
+int func(a) {
+  int x = (a ~/ 10) + (a % 10) % 2;
+  return x;
+}
 
-void main() {}
+void main() {
+  print(func(34));
+}
