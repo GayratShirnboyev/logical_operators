50166c9d919fc4a4301e967acc976cddc041ed16
G'ayrat <shirnboyevgayrat@gmail.com>
2022-07-01 14:16:56 +0500

    main16

--- bin/logical_operators16.dart
+++ bin/logical_operators16.dart
@@ -7,5 +7,11 @@ Given integer a,  check the following statement "The integer is a five-digit num
     Returns:
         bool: answer
 */
+// ignore: curly_braces_in_flow_control_structures
+bool func(int a) {
+  return a > 9999 && a < 100000;
+}
 
-void main() {}
+void main() {
+  print(func(34567));
+}
